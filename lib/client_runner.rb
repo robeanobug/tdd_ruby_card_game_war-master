9b98c6524e07b4a618f3f083b755e30d48a7075e
robeanobug <fakeemail@gmail.com>
2025-06-04 17:02:22 -0400

    Initial commit

---
+++ lib/client_runner.rb
@@ -0,0 +1,19 @@
+require 'socket'
+
+socket = TCPSocket.new('localhost', 3336)
+while true do
+  output = ""
+  until output != ""
+    begin
+      sleep(0.1)
+      output = socket.read_nonblock(1000).chomp # not gets which blocks
+    rescue IO::WaitReadable
+    end
+  end
+  if output.include?(":")
+    print output + ' '
+    socket.puts(gets.chomp)
+  else
+    puts output
+  end
+end
